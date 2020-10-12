c9fe145df97902f5947570c712718bfa1d38f68d
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-10-12 14:04:41 -0700

    init commit

---
+++ main.rb
@@ -0,0 +1,8 @@
+#main will require all other files
+
+require './player.rb'
+require './gamestate.rb'
+require './questions.rb'
+require './messageclass.rb'
+require './turns.rb'
+require './questionbank.rb'
